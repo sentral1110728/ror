7bba3ccfde3bff8f46d8cb102baa70ce3f409943
sentral1110728 <d3rtdr3zfh_0221@yahoo.co.jp>
2020-05-23 13:04:57 +0900

    article作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_024721) do
+ActiveRecord::Schema.define(version: 2020_05_23_035903) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
